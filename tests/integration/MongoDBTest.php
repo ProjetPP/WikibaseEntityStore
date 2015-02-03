0f95c836f77dcd77c553e935c074c58d2fb323e0
Tpt <thomaspt@hotmail.fr>
2015-02-03 14:42:50 +0100

    Add integration test for MongoDb

---
+++ tests/integration/MongoDBTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Wikibase\EntityStore;
+
+use Doctrine\MongoDB\Connection;
+use RuntimeException;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\EntityStore\Console\CliApplicationFactory;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Wikibase\EntityStore\MongoDB\MongoDBEntityStore;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Thomas Pellissier Tanon
+ */
+class MongoDBTest extends \PHPUnit_Framework_TestCase {
+
+	public function testMongoDbStore() {
+		$this->setupMongoDB();
+
+		$store = new MongoDBEntityStore( $this->getEntityCollection() );
+
+		$this->assertEquals(
+			new ItemId( 'Q1' ),
+			$store->getItemLookup()->getItemForId( new ItemId( 'Q1' ) )->getId()
+		);
+
+		$items = $store->getItemForTermLookup()->getItemsForTerm( new Term( 'en', 'universe') );
+		$this->assertEquals(
+			new ItemId( 'Q1' ),
+			$items[0]->getId()
+		);
+	}
+
+	private function setupMongoDB() {
+		$applicationFactory = new CliApplicationFactory();
+		$importCommand = $applicationFactory->newApplication()->find( 'mongodb:import-json-dump' );
+		$input = new ArrayInput( array(
+			'command' => 'mongodb:import-json-dump',
+			'file' => __DIR__ . '/../data/valid.json'
+		) );
+		$importCommand->run( $input, new NullOutput() );
+	}
+
+	private function getEntityCollection() {
+		$connection = new Connection( '' );
+		if( !$connection->connect() ) {
+			throw new RuntimeException( 'Fail to connect to the database' );
+		}
+
+		return $connection
+			->selectDatabase( 'wikibase' )
+			->selectCollection( 'entity' );
+	}
+}
